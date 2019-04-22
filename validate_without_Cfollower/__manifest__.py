d52f604f0720cd895bca31a61dc004b00a109ea6
William <williamcolin954@gmail.com>
2019-04-22 17:29:59 -0500

    Correction field

--- validate_without_Cfollower/__manifest__.py
+++ validate_without_Cfollower/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "tvpromocrm",
+    'name': "validate_without_Cfollower",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,14 +10,14 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Xmarts",
+    'website': "http://www.xmarts.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Adecuacion',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base','sale'],
