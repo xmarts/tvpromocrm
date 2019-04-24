a22f5f8042cfc8fb9dbbe0ad905ea0eb5647a773
Marco <marcocaaguilar@gmail.com>
2019-04-24 12:59:54 -0500

    Validaciones para seguidores

--- validate_without_Cfollower/models/models.py
+++ validate_without_Cfollower/models/models.py
@@ -5,14 +5,17 @@ from odoo import models, fields, api
 class CrmFollowersSale(models.Model):
 	_inherit = 'sale.order'
 
+	check_cust_followers = fields.Boolean(string='Check custom followers')
+
 	@api.multi
 	def action_confirm(self):
 		if self._get_forbidden_state_confirm() & set(self.mapped('state')):
 			raise UserError(_(
 				'It is not allowed to confirm an order in the following states: %s'
 			) % (', '.join(self._get_forbidden_state_confirm())))
-		'''for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
-			order.message_subscribe([order.partner_id.id])'''
+		if self.check_cust_followers:
+			for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
+				order.message_subscribe([order.partner_id.id])
 		self.write({
 			'state': 'sale',
 			'confirmation_date': fields.Datetime.now()
@@ -25,10 +28,13 @@ class CrmFollowersSale(models.Model):
 class CrmFollowersAccount(models.Model):
 	_inherit = 'account.invoice'
 
+	check_cust_followers = fields.Boolean(string='Check custom followers')
+
 	@api.multi
 	def invoice_validate(self):
 		for invoice in self.filtered(lambda invoice: invoice.partner_id not in invoice.message_partner_ids):
-			# invoice.message_subscribe([invoice.partner_id.id])
+			if self.check_cust_followers:
+				invoice.message_subscribe([invoice.partner_id.id])
 			# Auto-compute reference, if not already existing and if configured on company
 			if not invoice.reference and invoice.type == 'out_invoice':
 				invoice.reference = invoice._get_computed_reference()
